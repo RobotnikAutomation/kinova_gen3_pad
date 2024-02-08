db71516f7946fea7490e29f9736ca11b9669f1b4
MariaBeneytoROB <mbeneyto@robotnik.es>
2024-02-08 17:35:24 +0100

    add new functionalities: - open close gripper stopping at any position - move base - move in cartesian with respect to end effector

--- kinova_gen3_pad_plugin/include/kinova_gen3_pad_plugin/kinova_arm_gen3_plugin.h
+++ kinova_gen3_pad_plugin/include/kinova_gen3_pad_plugin/kinova_arm_gen3_plugin.h
@@ -4,6 +4,13 @@
 #include <kortex_driver/TwistCommand.h>
 #include <robotnik_pad/generic_pad_plugin.h>
 #include <kinova_gen3_pad_msgs/KinovaArmStatus.h>
+#include <kortex_driver/Base_JointSpeeds.h>
+#include <kortex_driver/JointSpeed.h>
+#include <kortex_driver/SendGripperCommand.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <Eigen/Dense>
+#include <std_msgs/Empty.h>
 
 namespace pad_plugins
 {
@@ -20,17 +27,22 @@ public:
 
 protected:
   std::string arm_control_topic_name_;
+  std::string arm_base_joint_control_topic_name_;
+  std::string arm_stop_motion_topic_name_;
   std::string set_home_service_name_;
+  std::string gripper_command_service_name_;
+  std::string base_frame_;
+  std::string tool_frame_;
   double max_linear_speed_, max_angular_speed_;
-  int button_deadman_, button_movement_deadman_, button_home_arm_;
+  int button_deadman_, button_movement_deadman_, button_home_arm_, button_rot_base_pos_, button_rot_base_neg_, button_open_gripper_, button_close_gripper_;
   int axis_open_gripper_, axis_close_gripper_;
   int axis_linear_x_, axis_linear_y_, axis_linear_z_;
   int axis_angular_x_, axis_angular_y_, axis_angular_z_;
   int button_speed_up_, button_speed_down_;
 
-  ros::Publisher arm_control_pub_, pad_status_pub_;
+  ros::Publisher arm_control_pub_, arm_base_joint_control_pub_, pad_status_pub_, stop_motion_pub_;
 
-  ros::ServiceClient set_home_service_;
+  ros::ServiceClient set_home_service_, gripper_command_client_;
 
   kinova_gen3_pad_msgs::KinovaArmStatus arm_status_msg_;
 
@@ -43,8 +55,19 @@ protected:
   double min_velocity_level_;
   //! defines how much you can increase/decrease the max_velocity_level (Normally 0.1)
   double velocity_level_step_;
+  double max_base_speed_;
+
+  kortex_driver::Base_JointSpeeds arm_joint_control_msg_;
+  kortex_driver::JointSpeed arm_base_joint_control_msg_;
 
   kortex_driver::TwistCommand arm_control_msg_;
+  kortex_driver::SendGripperCommand gripper_command_;
+  bool gripper_pressed_flag_;
+
+  tf::TransformListener tf_listener_;
+  tf::StampedTransform transform_, transform_stored_;
+  geometry_msgs::TwistStamped transformed_twist_;
+  std_msgs::Empty stop_motion_;
 };
 }  // namespace pad_plugins
 #endif  // PAD_PLUGIN_ELEVATOR_H_
--- kinova_gen3_pad_plugin/src/kinova_arm_gen3_plugin.cpp
+++ kinova_gen3_pad_plugin/src/kinova_arm_gen3_plugin.cpp
@@ -18,10 +18,17 @@ void PadPluginKinovaArmGen3::initialize(const ros::NodeHandle& nh, const std::st
 
   arm_control_topic_name_ = "/robot/arm/in/cartesian_velocity";
   readParam(pnh_, "arm_control_topic_name", arm_control_topic_name_, arm_control_topic_name_, required);
+  arm_base_joint_control_topic_name_ = "/robot/j2s6s200_driver/in/joint_velocity";
+  readParam(pnh_, "arm_base_control_topic_name", arm_base_joint_control_topic_name_, arm_base_joint_control_topic_name_, required);
+  arm_stop_motion_topic_name_ = "/my_gen3/in/stop";
+  readParam(pnh_, "arm_stop_motion_topic_name", arm_stop_motion_topic_name_, arm_stop_motion_topic_name_, required);
   set_home_service_name_ = "/robot/arm/in/home_arm";
   readParam(pnh_, "set_home_service_name", set_home_service_name_, set_home_service_name_, required);
+  gripper_command_service_name_ = "/my_gen3/base/send_gripper_command";
+  readParam(pnh_, "gripper_command_service_name", gripper_command_service_name_, gripper_command_service_name_, required);
   readParam(pnh_, "max_linear_speed", max_linear_speed_, 2.0, required);
   readParam(pnh_, "max_angular_speed", max_angular_speed_, 3.0, required);
+  readParam(pnh_, "max_base_speed", max_base_speed_, 0.5, required);
   readParam(pnh_, "config/button_deadman", button_deadman_, 4, required);
   readParam(pnh_, "config/button_movement_deadman", button_movement_deadman_, 5, required);
   readParam(pnh_, "config/button_home_arm", button_home_arm_, 12, required);
@@ -35,15 +42,24 @@ void PadPluginKinovaArmGen3::initialize(const ros::NodeHandle& nh, const std::st
   readParam(pnh_, "config/axis_angular_z_ee", axis_angular_z_, 9, required);
   readParam(pnh_, "config/button_speed_up", button_speed_up_, 3, required);
   readParam(pnh_, "config/button_speed_down", button_speed_down_, 1, required);
+  readParam(pnh_, "config/button_rotate_base_pos", button_rot_base_pos_, 9, required);
+  readParam(pnh_, "config/button_rotate_base_neg", button_rot_base_neg_, 10, required);
+  readParam(pnh_, "config/button_open_gripper", button_open_gripper_, 0, required);
+  readParam(pnh_, "config/button_close_gripper", button_close_gripper_, 2, required);
 
   // Publishers
   arm_control_pub_ = nh_.advertise<kortex_driver::TwistCommand>(arm_control_topic_name_, 10);
+  arm_base_joint_control_pub_ = nh_.advertise<kortex_driver::Base_JointSpeeds>(arm_base_joint_control_topic_name_, 10);
   pad_status_pub_ = pnh_.advertise<kinova_gen3_pad_msgs::KinovaArmStatus>("status", 10);
+  stop_motion_pub_ = pnh_.advertise<std_msgs::Empty>(arm_stop_motion_topic_name_, 1);
 
   // Services
   //set_home_service_ = nh_.serviceClient<kinova_msgs::HomeArm>(set_home_service_name_);
-
+  gripper_command_client_ = nh_.serviceClient<kortex_driver::SendGripperCommand>(gripper_command_service_name_);
+  gripper_command_client_.waitForExistence();
+  
   // initialize variables
+  gripper_pressed_flag_ = false;
   current_velocity_level_ = 0.1;
   velocity_level_step_ = 0.1;
   max_velocity_level_ = 1.0;
@@ -51,6 +67,16 @@ void PadPluginKinovaArmGen3::initialize(const ros::NodeHandle& nh, const std::st
   fingers_closure_percentage_ = 0.0;
   arm_control_msg_ = kortex_driver::TwistCommand();
   arm_status_msg_ = kinova_gen3_pad_msgs::KinovaArmStatus();
+  arm_joint_control_msg_ = kortex_driver::Base_JointSpeeds();
+  arm_joint_control_msg_.joint_speeds.resize(1);
+  arm_base_joint_control_msg_ = kortex_driver::JointSpeed();
+  arm_base_joint_control_msg_.joint_identifier = 0;
+  arm_joint_control_msg_.joint_speeds[0] = arm_base_joint_control_msg_;
+  gripper_command_ = kortex_driver::SendGripperCommand();
+  gripper_command_.request.input.mode = 2;
+  base_frame_ = "robot_arm_base_link";
+  tool_frame_ = "robot_arm_end_effector_link";
+  stop_motion_ = std_msgs::Empty();
 }
 
 void PadPluginKinovaArmGen3::execute(const std::vector<Button>& buttons, std::vector<float>& axes)
@@ -68,35 +94,144 @@ void PadPluginKinovaArmGen3::execute(const std::vector<Button>& buttons, std::ve
       ROS_INFO("PadPluginKinovaArmGen3::execute: speed up -> velocity level = %.1f%%", current_velocity_level_ * 100.0);
     }
 
-    /*if (buttons[button_home_arm_].isReleased())
+    if (buttons[button_rot_base_pos_].isPressed())
     {
-      kinova_msgs::HomeArm srv_msg;
-      set_home_service_.call(srv_msg);
-      ROS_INFO("PadPluginKinovaArmGen3::execute: set home");
-    }*/
+       arm_joint_control_msg_.joint_speeds[0].value = max_base_speed_;
+    }
 
-    /*fingers_closure_percentage_ -= (axes[axis_open_gripper_] + 1) / 4;
-    fingers_closure_percentage_ += (axes[axis_close_gripper_] + 1) / 4;
-    if (fingers_closure_percentage_ > 100) {
-      fingers_closure_percentage_ = 100;
-    } else if (fingers_closure_percentage_ < 0) {
-      fingers_closure_percentage_ = 0;
-    }*/
-    
-    arm_control_msg_.twist.linear_x = current_velocity_level_ * max_linear_speed_ * axes[axis_linear_x_];
-    arm_control_msg_.twist.linear_y = current_velocity_level_ * max_linear_speed_ * axes[axis_linear_y_];
-    arm_control_msg_.twist.linear_z = current_velocity_level_ * max_linear_speed_ * axes[axis_linear_z_];
+    if (buttons[button_rot_base_neg_].isPressed())
+    {
+       arm_joint_control_msg_.joint_speeds[0].value = -max_base_speed_;
+    }
+
+    if (buttons[button_rot_base_pos_].isReleased())
+    {
+       stop_motion_pub_.publish(stop_motion_);
+       arm_joint_control_msg_.joint_speeds[0].value = 0.0;
+    }
+
+    if (buttons[button_rot_base_neg_].isReleased())
+    {
+       stop_motion_pub_.publish(stop_motion_);
+       arm_joint_control_msg_.joint_speeds[0].value = 0.0;
+    }
+
+    if (buttons[button_open_gripper_].isPressed())
+    {
+      if(gripper_pressed_flag_ == false)
+      { 
+        gripper_pressed_flag_= true;
+        kortex_driver::Finger finger;
+        finger.finger_identifier = 1;
+        finger.value = 0.5;
+        gripper_command_.request.input.gripper.finger.clear();
+        gripper_command_.request.input.gripper.finger.push_back(finger);
+        gripper_command_client_.call(gripper_command_);
+      }
+    }
+
+    if (buttons[button_open_gripper_].isReleased())
+    {
+        gripper_pressed_flag_= false;
+        kortex_driver::Finger finger;
+        finger.finger_identifier = 1;
+        finger.value = 0.0;
+        gripper_command_.request.input.gripper.finger.clear();
+        gripper_command_.request.input.gripper.finger.push_back(finger);
+        gripper_command_client_.call(gripper_command_);
+    }
+
+    if (buttons[button_close_gripper_].isPressed())
+    {
+      if(gripper_pressed_flag_ == false)
+      { 
+        gripper_pressed_flag_= true;
+        kortex_driver::Finger finger;
+        finger.finger_identifier = 1;
+        finger.value = -0.5;
+        gripper_command_.request.input.gripper.finger.clear();
+        gripper_command_.request.input.gripper.finger.push_back(finger);
+        gripper_command_client_.call(gripper_command_);
+      }
+    }
+
+    if (buttons[button_close_gripper_].isReleased())
+    {
+        gripper_pressed_flag_= false;
+        kortex_driver::Finger finger;
+        finger.finger_identifier = 1;
+        finger.value = 0.0;
+        gripper_command_.request.input.gripper.finger.clear();
+        gripper_command_.request.input.gripper.finger.push_back(finger);
+        gripper_command_client_.call(gripper_command_);
+    }
+
+
+
+
+   try {
+        // Get the transform from the tool frame to the base frame
+        tf_listener_.waitForTransform(base_frame_, tool_frame_, ros::Time(0), ros::Duration(1.0));
+        tf_listener_.lookupTransform(base_frame_, tool_frame_, ros::Time(0), transform_);
+
+
+        if(std::abs(axes[axis_linear_x_]) <= 0.3)
+        { 
+          axes[axis_linear_x_] = 0;
+
+        }
+        if(std::abs(axes[axis_linear_y_]) <= 0.3)
+        {
+          axes[axis_linear_y_] = 0;
+        }     
+        if(std::abs(axes[axis_linear_z_]) <= 0.3)
+        {
+          axes[axis_linear_z_] = 0;
+        }    
+
+        // Extract the rotation matrix from the transform
+        Eigen::Matrix3d rotation_matrix;
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                rotation_matrix(i, j) = transform_.getBasis()[i][j];
+
+        // Transform the linear velocities to the base frame using matrix multiplication
+        Eigen::Vector3d linear_velocities( axes[axis_linear_x_], axes[axis_linear_y_], axes[axis_linear_z_]);
+        Eigen::Vector3d rotated_linear_velocities = rotation_matrix * linear_velocities;
+
+        // Create a new TwistStamped message with the transformed velocities
+        transformed_twist_.twist.linear.x = rotated_linear_velocities.x();
+        transformed_twist_.twist.linear.y = rotated_linear_velocities.y();
+        transformed_twist_.twist.linear.z = rotated_linear_velocities.z();
+
+    } catch (tf::TransformException& ex) {
+        ROS_WARN("TF transform lookup failed: %s", ex.what());
+    }
+
+
+    arm_control_msg_.twist.linear_x = current_velocity_level_ * max_linear_speed_ * transformed_twist_.twist.linear.x;
+    arm_control_msg_.twist.linear_y = current_velocity_level_ * max_linear_speed_ * transformed_twist_.twist.linear.y;
+    arm_control_msg_.twist.linear_z = current_velocity_level_ * max_linear_speed_ * transformed_twist_.twist.linear.z;
 
     arm_control_msg_.twist.angular_x = current_velocity_level_ * max_angular_speed_ * axes[axis_angular_x_];
-    arm_control_msg_.twist.angular_y = current_velocity_level_ * max_angular_speed_ * axes[axis_angular_y_];
+    arm_control_msg_.twist.angular_y = - current_velocity_level_ * max_angular_speed_ * axes[axis_angular_y_];
     arm_control_msg_.twist.angular_z = - current_velocity_level_ * max_angular_speed_ * axes[axis_angular_z_] * 2;
 
-    
-    //arm_control_msg_.fingers_closure_percentage = fingers_closure_percentage_;
-    arm_control_pub_.publish(arm_control_msg_);
+
+
+    if( arm_joint_control_msg_.joint_speeds[0].value != 0)
+    {
+      arm_base_joint_control_pub_.publish(arm_joint_control_msg_);
+      arm_joint_control_msg_.joint_speeds[0].value = 0.0;
+    }else{
+      arm_control_pub_.publish(arm_control_msg_);      
+      arm_joint_control_msg_.joint_speeds[0].value = 0.0;
+    }
   }
   else if (buttons[button_deadman_].isReleased())
   {
+    stop_motion_pub_.publish(stop_motion_);
+
     arm_control_msg_.twist.linear_x = 0.0;
     arm_control_msg_.twist.linear_y = 0.0;
     arm_control_msg_.twist.linear_z = 0.0;
@@ -106,6 +241,10 @@ void PadPluginKinovaArmGen3::execute(const std::vector<Button>& buttons, std::ve
     //arm_control_msg_.fingers_closure_percentage = fingers_closure_percentage_;
 
     arm_control_pub_.publish(arm_control_msg_);
+
+    arm_joint_control_msg_.joint_speeds[0].value = 0.0;
+
+    arm_base_joint_control_pub_.publish(arm_joint_control_msg_);
   }
 
   arm_status_msg_.velocity_level = current_velocity_level_ * 100;
